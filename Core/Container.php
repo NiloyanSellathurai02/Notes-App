b4e0c5a35cab47849c3b8b673415cbbf947700f1
NiloyanSellathurai02 <n.sella2001@gmail.com>
2023-12-03 23:39:52 +0100

    Not yet finished

--- Core/Container.php
+++ Core/Container.php
@@ -11,7 +11,7 @@ class Container
     }
 
 
-    public function resolve($key , )
+    public function resolve($key)
     {
         if (!array_key_exists($key , $this->bindings)){
             throw new \Exception("No matching binding found for {$key}");
--- controllers/notes/edit.php
+++ controllers/notes/edit.php
@@ -1,9 +1,23 @@
 <?php
 
-$errors=[];
+use Core\Database;
+use Core\App;
 
-view("notes/create.view.php", [
-    'heading' => 'Create Note',
-    'errors' => []
+$db = App::resolve(Database::class);
+
+$currentUserId= 1;
+
+
+$note= $db->query('Select * from notes where id = :id', [
+    'id' => $_GET['id']
+])->findOrFail();
+
+authorize($note['user_id'] === $currentUserId);
+
+
+view("notes/edit.view.php", [
+    'heading' => 'Edit Note',
+    'errors' => [],
+    "note" => $note
 ]);
 
